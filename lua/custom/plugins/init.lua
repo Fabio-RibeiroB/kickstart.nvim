b62d12c65de8b2c0b7209483a9f0b5e4c70bbe8a
fabio.brady@tui.co.uk <fabio.brady@tui.co.uk>
2025-06-22 12:56:16 +0100

    Feat: various bits of nvim config changes that I like

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,12 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+---- You can add your own plugins here or in other files in this directory!
+----  I promise not to create any merge conflicts in this directory :)
+----
+---- See the kickstart.nvim README for more information
+return {
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- You can add custom config here if needed
+    },
+  }
+}
